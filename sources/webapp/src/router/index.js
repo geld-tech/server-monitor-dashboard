1670aee739071adc2ef2e4cfcc232a604e06ecbd
zlig <gdevillard@gmail.com>
2018-07-11 12:07:55 +0100

    Remove unneeded from Router

--- sources/webapp/src/router/index.js
+++ sources/webapp/src/router/index.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import ProjectsTweets from '@/components/ProjectsTweets'
 import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'ProjectsTweets',
-      component: ProjectsTweets
-    },
     {
       path: '/404',
       name: '404',
