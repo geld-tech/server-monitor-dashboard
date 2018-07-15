db7671761400e11716ed864ec9a2c6c95334c7ff
zlig <gdevillard@gmail.com>
2018-07-15 21:09:34 +0100

    Improve local dev script

--- local-dev.sh
+++ local-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-echo -e "\n### CLEANUP & CONFIGURE ###"
+echo "### CLEANUP & CONFIGURE ###"
 # Cleanup
 rm -rf .local_dev/
 mkdir .local_dev/
@@ -16,33 +16,36 @@ find . -type f | xargs sed -i "s/__VERSION__/0.0.1/g"
 find . -type f | xargs sed -i "s/__DATE__/01-01-1970/g"
 
 # Flask application, enforce no syntax errors or undefined names, and flags other issues
-echo -e "\n### PYTHON FLAKE8 ###"
+echo ""
+echo "### PYTHON FLAKE8 ###"
 cd server/
 flake8 . --show-source --max-line-length=159 --max-complexity=10 --statistics --count
 cd ..
 
 # Build Vue application with DevTools enabled (Firefox or Chrome plugin)
-echo -e "\n### VUE DEVTOOLS ###"
+echo ""
+echo "### VUE DEVTOOLS ###"
 cd webapp/
 sed -i '/Vue.config.productionTip = false/a Vue.config.devtools = true' src/main.js
 
-echo -e "\n### NPM INSTALL ###"
+echo "### NPM INSTALL ###"
 npm install
 
-echo -e "\n### NPM LINT ###"
+echo "### NPM LINT ###"
 npm run lint
 
-echo -e "\n### NPM AUDIT ###"
+echo "### NPM AUDIT ###"
 set +e     # Ignores a particular command
 npm audit  2> /dev/null # Run conditionally as not installed on all systems
 set -e     # Then return to normal failures on all errors
 
-echo -e "\n### NPM BUILD ###"
+echo "### NPM BUILD ###"
 npm run build
 cd ..
 
 # Prepare application
-echo -e "\n### PREPARE ###"
+echo ""
+echo "### PREPARE ###"
 cd server/
 mkdir templates/
 mkdir static/
@@ -55,6 +58,7 @@ if [ ! -f server/config/settings.cfg ]; then
 fi
 
 # Run application locally on port :5000 (Press CTRL+C to quit)
-echo -e "\n### RUN ###"
+echo ""
+echo "### RUN ###"
 cd server/
 python application.py
