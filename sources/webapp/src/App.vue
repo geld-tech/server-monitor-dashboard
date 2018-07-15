bcad7cd21538f8d9e90089069d426dd01dee72c2
zlig <gdevillard@gmail.com>
2018-07-15 16:18:31 +0100

    Display temperature and platform

--- sources/webapp/src/App.vue
+++ sources/webapp/src/App.vue
@@ -38,6 +38,22 @@
                 <p v-if="data">{{ data.hostname }}</p>
             </b-col>
         </b-row>
+        <b-row align-v="start" align-h="around">
+            <b-col sm="4">
+                <h6>Platform</h6>
+            </b-col>
+            <b-col sm="4">
+                <p v-if="data">{{ data.platform }}</p>
+            </b-col>
+        </b-row>
+        <b-row align-v="start" align-h="around">
+            <b-col sm="4">
+                <h6>Temperature</h6>
+            </b-col>
+            <b-col sm="4">
+                <p v-if="data">{{ data.cpu_temp }}</p>
+            </b-col>
+        </b-row>
     </b-container>
     <div id="app-container">
         <router-view></router-view>
