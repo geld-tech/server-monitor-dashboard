93d2e797343dbdc4d90188ea7bb850196f506c20
zlig <gdevillard@gmail.com>
2018-07-18 14:21:15 +0100

    Implements simple service

---
+++ sources/server/monitor-collectord.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import time
+from daemon import Daemon
+
+class Metrics(object):
+    def run(self):
+        while True:
+            time.sleep(10)
+
+
+class MetricsCollector(Daemon):
+    def run(self):
+        server = Metrics()
+        server.run()
+
+
+if __name__ == "__main__":
+    daemon = MetricsCollector('/var/run/monitor-collectord.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
