65f063136b7787279a715fae393b8a7b88579e0e
zlig <gdevillard@gmail.com>
2018-07-24 01:15:35 +0100

    Ensure compatibility with some virtual machines

--- sources/server/monitor-collectord.py
+++ sources/server/monitor-collectord.py
@@ -13,8 +13,8 @@ from sqlalchemy.orm import sessionmaker
 class MetricsCollector():
     def __init__(self, pid_file, poll_interval=30, db_path='/dev/shm/monitor-collectord.sqlite3'):
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
         self.pidfile_timeout = 5
         self.pidfile_path = pid_file
         self.poll_interval = poll_interval
