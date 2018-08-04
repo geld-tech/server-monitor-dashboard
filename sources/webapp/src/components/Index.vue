1186219d4f4c45acc1081b44a471a46cecf5b766
zlig <gdevillard@gmail.com>
2018-08-04 14:55:19 +0100

    Hide label of CPU temperature if not available

--- sources/webapp/src/components/Index.vue
+++ sources/webapp/src/components/Index.vue
@@ -49,12 +49,12 @@
                     </b-progress>
                 </b-col>
             </b-row>
-            <b-row align-v="start" align-h="around">
+            <b-row v-if="data && data.cpu_temp !== false && parseInt(data.cpu_temp) > 0" align-v="start" align-h="around">
                 <b-col sm="4">
                     <h5>Temperature</h5>
                 </b-col>
                 <b-col sm="8">
-                    <b-progress show-progress v-if="data && data.cpu_temp !== false && parseInt(data.cpu_temp) > 0" v-bind:max="100" class="w-80 mb-2">
+                    <b-progress show-progress v-bind:max="100" class="w-80 mb-2">
                         <b-progress-bar variant="primary" v-bind:value="data.cpu_temp" v-bind:label="data.cpu_temp+'&deg; C'" height="20px"></b-progress-bar>
                     </b-progress>
                 </b-col>
